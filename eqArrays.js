d97cb9f208159cb7e3a07c16f11495b94479ef2d
Christine Lo <christine.lo.hayes@gmail.com>
2021-09-14 21:56:16 -0400

    Implement eqArrays

---
+++ eqArrays.js
@@ -0,0 +1,26 @@
+// FUNCTION IMPLEMENTATION
+const eqArrays = function(actual, expected) {
+  if (actual.length !== expected.length) {
+    return false;
+  }
+
+  for (let i = 0; i < actual.length; i++) {
+    if (actual[i] !== expected[i]) {
+      return false;
+    }
+  }
+
+  return true;
+};
+
+const assertEqual = function(actual, expected) {
+  if (actual === expected) {
+    console.log(`✅✅✅  Assertion Passed: ${actual} === ${expected}`);
+  } else {
+    console.log(`🛑🛑🛑 Assertion Failed: ${actual} !== ${expected}`);
+  }
+};
+
+assertEqual(eqArrays([1, 2, 3], [1, 2, 3]), true);
+assertEqual(eqArrays([1, 2], [1, 2, 3]), false);
+assertEqual(eqArrays([], []), true);
