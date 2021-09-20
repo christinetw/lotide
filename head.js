f2a4127511e9a16c392e7c2c6d559f88f4337e8f
Christine Lo <christine.lo.hayes@gmail.com>
2021-09-20 15:00:17 -0400

    convert head tests to mocha and chai

--- head.js
+++ head.js
@@ -1,10 +1,3 @@
-const assertEqual = function(actual, expected) {
-  if (actual === expected) {
-    console.log(`✅✅✅  Assertion Passed: ${actual} === ${expected}`);
-  } else {
-    console.log(`🛑🛑🛑 Assertion Failed: ${actual} !== ${expected}`);
-  }
-};
 
 const head = function(array) {
   if (array === undefined || array.length === 0) {
@@ -14,5 +7,4 @@ const head = function(array) {
   }
 };
 
-assertEqual(head([5, 6, 7]), 5);
-assertEqual(head(["Hello", "Lighthouse", "Labs"]), "Hello");
+module.exports = head;
