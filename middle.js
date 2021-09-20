16b45b53d1c14eeab0f39f193be86f14b5965d90
Christine Lo <christine.lo.hayes@gmail.com>
2021-09-20 18:59:36 -0400

    middle.js

--- middle.js
+++ middle.js
@@ -1,39 +1,15 @@
 
+const assertArraysEqual = require("./assertArraysEqual");
 
-
-const middle = function(array) {
+const middle = function (array) {
 
   if (array.length === 1 || array.length === 2) {
     return [];
   } else if (array.length % 2 === 0) {
     return array.slice((array.length / 2) - 1, (array.length / 2) + 1);
   } else if (array.length % 2 !== 0) {
-    return array.slice(array.length / 2,(array.length / 2) + 1,);
-  }
+    return array.slice(array.length / 2, (array.length / 2) + 1,);
+  };
+}
 
-};
-
-const eqArrays = function(actual, expected) {
-  if (actual.length !== expected.length) {
-    return false;
-  }
-
-  for (let i = 0; i < actual.length; i++) {
-    if (actual[i] !== expected[i]) {
-      return false;
-    }
-  }
-
-  return true;
-};
-
-const assertArraysEqual = function(actual, expected) {
-  if (eqArrays(actual, expected) === true) {
-    console.log(`✅✅✅  Assertion Passed: ${actual} === ${expected}`);
-  } else {
-    console.log(`🛑🛑🛑 Assertion Failed: ${actual} !== ${expected}`);
-  }
-};
-
-assertArraysEqual(middle([1, 2, 3, 4, 5, 6]), [3, 4]);
-assertArraysEqual(middle([1, 2, 3, 4, 5]), [3]);
+module.exports = middle;
